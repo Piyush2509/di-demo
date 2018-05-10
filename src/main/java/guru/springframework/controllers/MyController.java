8bd9dcd987131fd08de263293017e3a27099bc53
Piyush2509 <piyush.jayaswal@gmail.com>
2018-05-10 12:38:48 -0400

    Referring bean from context

---
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author piyush.b.kumar
+ *
+ */
+@Controller
+public class MyController {
+	
+	public void hello() {
+		System.out.println("Hello!");
+	}
+
+}
